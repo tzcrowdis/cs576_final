2632ee14e586c105eb994c5f43dbff797db87dfc
tzcrowdis <tcrowdis@umass.edu>
2022-12-07 17:37:48 -0500

    Navmesh improvement
    Done all i can do until character controller is finished

--- The Apple Company/Assets/npc Assets/Scripts/robotController.cs
+++ The Apple Company/Assets/npc Assets/Scripts/robotController.cs
@@ -8,14 +8,21 @@ public class robotController : MonoBehaviour
     private Vector3 job1;
     private Vector3 job2;
     private Vector3 curr_dest;
+    private float curr_time;
+    private float work_time;
 
     public NavMeshAgent robot;
+    public Animator robot_anim;
 
     void Start()
     {
         //get position of the two jobs
         job1 = GameObject.Find("Job 1").transform.position;
         job2 = GameObject.Find("Job 2").transform.position;
+
+        //how long is a job
+        work_time = 5.0f;
+        curr_time = 0;
 
         //use randomness to pick first job position
         if (Random.Range(0f, 1f) <= 0.5)
@@ -29,6 +36,27 @@ public class robotController : MonoBehaviour
         //traverse to current job
         robot.SetDestination(curr_dest);
 
-        //remember to set walking to true for animation
+        //control animations and assign new job
+        if (robot.velocity.magnitude > 0)
+            robot_anim.SetBool("walking", true);
+        else
+        {
+            robot_anim.SetBool("walking", false);
+            if (curr_time <= 0)
+                curr_time = work_time;
+        }
+
+        //working timer
+        if (curr_time > 0)
+            curr_time -= Time.deltaTime;
+
+        //job timer up assign new job
+        if (curr_time <= 0 && robot.velocity.magnitude == 0)
+        {
+            if (Random.Range(0f, 1f) <= 0.5)
+                curr_dest = job1;
+            else
+                curr_dest = job2;
+        }
     }
 }
