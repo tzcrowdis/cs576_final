c5602839fc098d4e696cef23a961554457be8ea4
tzcrowdis <tcrowdis@umass.edu>
2022-12-16 19:59:18 -0500

    Animations and minor updates

--- The Apple Company/Assets/npc Assets/Scripts/robotController.cs
+++ The Apple Company/Assets/npc Assets/Scripts/robotController.cs
@@ -76,7 +76,8 @@ public class robotController : MonoBehaviour
         }
         else //in conversation
         {
-            //lock movement
+            //lock movement and anim
+            robot_anim.SetBool("walking", false);
             robot.SetDestination(transform.position);
             //rotate to player (from unity api)
             Vector3 targetDirection = target.position - transform.position;
