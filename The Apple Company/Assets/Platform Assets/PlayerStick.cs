7fbc19f2dcd1a136141eebdf7c59ceb2551733ed
tzcrowdis <tcrowdis@umass.edu>
2022-12-19 18:36:16 -0500

    minor updates

--- The Apple Company/Assets/Platform Assets/PlayerStick.cs
+++ The Apple Company/Assets/Platform Assets/PlayerStick.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class PlayerStick : MonoBehaviour
 {
-    //source: https://www.youtube.com/watch?v=rO19dA2jksk
-
     public GameObject Player;
     
     void OnTriggerEnter(Collider other)
--- The Apple Company/Assets/Player Assets/Scripts/CameraHolder.cs
+++ The Apple Company/Assets/Player Assets/Scripts/CameraHolder.cs
@@ -6,11 +6,6 @@ public class CameraHolder : MonoBehaviour
 {
 
     public Transform cameraPosition;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
